d4c9bef7b37d82ff92e7324c7a68e046281d945c
Bakugou808 <janu.m.sung@gmail.com>
2020-04-19 16:43:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, eventName) { 
+    let msgs = []
+    for (let i = 0; i < names.length; i++) {
+        let msg = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+        msgs.push(msg)
+    }
+    return msgs 
+}
+
+names = ["Ada", "Brendan", "Ali"]
+events = "birthday"
+
+writeCards(names, events)
+
+function countDown(num) {
+    let i = num 
+    while (i > -1) {
+        console.log(i);
+        i = i-1
+    }
+    
+}
